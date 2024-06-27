ed9899f50cef8c70f8291fe3507090fcd84bd14e
John Strunk <developer@jrstrunk.com>
2024-06-27 15:51:46 -0400

    Improve downscale image interface

--- src/downscale_image.py
+++ src/downscale_image.py
@@ -1,13 +1,49 @@
 from PIL import Image
 from pathlib import Path
 
-def convert_to_webp(source):
+def downscale(source, dest, size, quality):
+    # Open the image
+    image = Image.open(source)
+
+    # Resize the image
+    image = __resize_image(image, size)
+
+    # Reduce the color depth of the image
+    image = __reduce_color_depth(image)
+
+    # Save the image in WebP format with reduced quality
+    destination = dest.with_suffix(".webp")
+    image.save(destination, format="webp", quality=quality)
+
+def __resize_image(image, target_size):
+    original_width, original_height = image.size
+
+    if original_width > original_height:
+        new_height = target_size
+        new_width = int(target_size * (original_width / original_height))
+    else:
+        new_width = target_size
+        new_height = int(target_size * (original_height / original_width))
+
+    image.thumbnail((new_width, new_height), Image.Resampling.LANCZOS)
+
+    return image
+
+def __reduce_color_depth(image):
+    # idk if this really helps
+    # image_with_reduced_color_depth = image.convert("P", palette=Image.ADAPTIVE, colors=256)
+    # return image_with_reduced_color_depth
+    return image
+
+def __convert_to_webp(source):
+    """Just converts the image to webp"""
     destination = source.with_suffix(".webp")
     image = Image.open(source)
     image.save(destination, format="webp")
     return destination
 
-def resize_crop_image(image, target_size):
+def __resize_crop_image(image, target_size):
+    """Don't think we want to ever crop an image, but this is here"""
     original_width, original_height = image.size
     target_width, target_height = target_size
 
@@ -27,44 +63,3 @@ def resize_crop_image(image, target_size):
     resized_image = cropped_image.resize(target_size, Image.Resampling.LANCZOS)
 
     return resized_image
-
-def resize_image(image, target_size):
-    original_width, original_height = image.size
-
-    if original_width > original_height:
-        new_height = target_size
-        new_width = int(target_size * (original_width / original_height))
-    else:
-        new_width = target_size
-        new_height = int(target_size * (original_height / original_width))
-
-    image.thumbnail((new_width, new_height), Image.Resampling.LANCZOS)
-
-    return image
-
-def reduce_color_depth(image):
-    # idk if this really helps
-    # image_with_reduced_color_depth = image.convert("P", palette=Image.ADAPTIVE, colors=256)
-    # return image_with_reduced_color_depth
-    return image
-
-def save_image_with_reduced_quality(image, destination, quality):
-    image.save(destination, format="webp", quality=quality)
-    return destination
-
-def downscale(source, dest, size, quality):
-    # Open the image
-    image = Image.open(source)
-
-    # Resize the image
-    image = resize_image(image, size)
-
-    # Reduce the color depth of the image
-    image = reduce_color_depth(image)
-
-    # Save the image in WebP format with reduced quality
-    destination = dest.with_suffix(".webp")
-    save_image_with_reduced_quality(image, destination, quality)
-
-    return destination
-    
