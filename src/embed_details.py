e3e689d41acb7d47b7ed02f21ada081689bcff90
John Strunk <developer@jrstrunk.com>
2024-07-09 12:24:41 -0400

    Add function to embed the generated image description into the image metadata

--- src/embed_details.py
+++ src/embed_details.py
@@ -1,10 +1,16 @@
 from PIL import Image
 import io
 
-def into_image(original_img, images_to_embed, quality):
-    # Prepare the original image data
+def into_image(original_img, images_to_embed, quality, exif_data):  
+    # Save the image to an in-memory buffer
     original_buffer = io.BytesIO()
-    original_img.save(original_buffer, format="WEBP", quality=quality)
+
+    original_img.save(
+        original_buffer, 
+        format="WEBP", 
+        quality=quality, 
+        exif=exif_data or b"", # we can't just pass None, so pass an empty byte string instaed
+    )
 
     combined_data = original_buffer.getvalue()
 
